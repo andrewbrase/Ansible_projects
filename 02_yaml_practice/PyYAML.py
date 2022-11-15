9915d853732afedf50035e89625a652870e1197c
Brase <brasea@wwt.com>
2022-11-15 15:32:22 -0600

    worked on pyyaml

--- 02_yaml_practice/PyYAML.py
+++ 02_yaml_practice/PyYAML.py
@@ -10,25 +10,46 @@ with open('PyYAML.yml') as f:
     # use the load_all function - it becomes a python dictionary
     yaml_contents = yaml.load_all(f, Loader=yaml.FullLoader)
     # for every item in that dictionary -
-    for yaml_content in yaml_contents:  
+    for yaml_content in yaml_contents:
         # seperate the keys and values
         for key, value in yaml_content.items():
             # print the key and value
             print(f"{key}: {value}")
     print("\n\n")
 
-# The REVERSE of this is converting this python dictionary into a YAML format 
+# The REVERSE of this is converting this python dictionary into a YAML format
 # we achieve that using the dump() function
 
-# this method will serialize a Python object into 
-# a YAML stream, where the Python obect could 
+# this method will serialize a Python object into
+# a YAML stream, where the Python obect could
 # be a dictionary
 
-cardict = {
-    "brand":"Ford",
-    "model":"mustang",
-    "year":1964 
-}
+cardict = [["this","is","a","list"],{
+    "car": {
+        "brand": "Ford",
+        "model": "mustang",
+        "year": 1964
+    }
+},
+    {
+    "factories": {
+        "building_01": "Illinois",
+        "building_02": "Michigan",
+        "building_03": "Wisconsin"
+    }
+}]
+
+# - - this
+#   - is
+#   - a
+#   - list
+# - brand: Ford
+#   model: mustang
+#   year: 1964
+# - factories:
+#     building_01: Illinois
+#     building_02: Michigan
+#     building_03: Wisconsin
 
 print(yaml.dump(cardict))
 print(type(yaml.dump(cardict)))
@@ -37,5 +58,32 @@ print(type(yaml.dump(cardict)))
 # brand: Ford
 # model: mustang
 # year: 1964
+
+# <class 'str'>
+
+# ___
+# name: Martin
+# job: Developer
+# employed: True
+# foods: ['Apple', 'Orange', 'Strawberry', 'Mango']
+# languages: {'Python': 'good', 'Javascript': 'good', 'Rust': 'good'}
+# education: 4 GCSEs
+# 3 A-Levels
+# BSc in the IoT
+
+
+
+# - - this
+#   - is
+#   - a
+#   - list
+# - car:
+#     brand: Ford
+#     model: mustang
+#     year: 1964
+# - factories:
+#     building_01: Illinois
+#     building_02: Michigan
+#     building_03: Wisconsin
 
 # <class 'str'>
